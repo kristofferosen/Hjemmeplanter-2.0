e5fef48479bf8cdcc9aac09a33040d47340c2800
kristofferosen <kristofferosen@gmail.com>
2017-09-19 20:35:33 +0200

    sfjhgkdfg

--- models/group.js
+++ models/group.js
@@ -2,6 +2,19 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 var Mote = require('./mote');
 
+var DataPoint2Schema   = new Schema({
+    group: String,
+    mote: String,
+    soilMoisture : String,
+    soilTemperature : String,
+    time : Date
+});
+
+var MoteSchema   = new Schema({
+	datapoints: [DataPoint2Schema],
+    moteNr: String
+});
+
 var Groupschema  = new Schema({
 	motes: [Mote],
     areaNr: String
--- models/mote.js
+++ models/mote.js
@@ -1,9 +1,16 @@
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
-var Datapoint2 = require('./datapoint2');
 
+
+var DataPoint2Schema   = new Schema({
+    group: String,
+    mote: String,
+    soilMoisture : String,
+    soilTemperature : String,
+    time : Date
+});
 var MoteSchema   = new Schema({
-	datapoints: [Datapoint2],
+	datapoints: [DataPoint2Schema],
     moteNr: String
 });
 
