8129dec0dc447c357592f9a36f78d16a4392a23f
kristofferosen <kristofferosen@gmail.com>
2017-09-19 13:38:33 -0400

    sdfsdf

--- models/group.js
+++ models/group.js
@@ -1,10 +1,11 @@
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
-var Mote = require('./models/mote');
+var Mote = require('./mote');
 
 var Groupschema  = new Schema({
 	motes: [Mote],
     areaNr: String,
     average: [Number]
+});
 
 module.exports = mongoose.model('Group', Groupschema);
--- models/mote.js
+++ models/mote.js
@@ -1,10 +1,10 @@
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
-var Datapoint2 = require('./models/datapoint');
+var Datapoint2 = require('./datapoint2');
 
 var MoteSchema   = new Schema({
-	datapoints: [Datapoint2]
-    moteNr: String,
+	datapoints: [Datapoint2],
+        moteNr: String
 });
 
 module.exports = mongoose.model('Mote', MoteSchema);
